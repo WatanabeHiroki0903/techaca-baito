29fb7dfa678341ed827cc30206cef5107f54ce96
Hiroki Watanabe <hiroki_watanabe@lockon.co.jp>
2017-03-13 18:09:21 +0900

    初回コミット

---
+++ eccube-2.13.5/data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost:41549/');
+define('HTTPS_URL', 'http://localhost:41549/');
+define('ROOT_URLPATH', '/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'soccer0903');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'eccube');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'crawiokiabaeclilaiueprapuvouueraegaidaep');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
